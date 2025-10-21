4477cd4de96d60407e52348325662c344f0ae61e
IlijaIv <ivanovicilija04@gmail.com>
2025-10-21 20:03:37 +0200

    com3

--- podwave/src/App.tsx
+++ podwave/src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <>PodWave</>;
-}
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Podcasts from './pages/Podcasts';
+import PodcastDetails from './pages/PodcastDetails';
+import Favorites from './pages/Favorites';
+import NotFound from './pages/NotFound';
 
-export default App;
+export default function App() {
+  return (
+    <div className='min-h-full p-6'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/podcasts' element={<Podcasts />} />
+        <Route path='/podcasts/:podcastId' element={<PodcastDetails />} />
+        <Route path='/favorites' element={<Favorites />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
